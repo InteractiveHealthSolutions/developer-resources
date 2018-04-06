1e6543a511960b0dac2ced56c7d647c821a8d3b7
MUHAMMAD WAQAS <muhammad.waqas@ihsinformatics.com>
2018-04-06 16:29:09 +0500

    test cases for date class

--- utilities/test/com/ihsinformatics/DateTimeUtilTest.java
+++ utilities/test/com/ihsinformatics/DateTimeUtilTest.java
@@ -1,6 +1,6 @@
 package com.ihsinformatics;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.security.InvalidParameterException;
 import java.util.Arrays;
@@ -13,8 +13,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.ihsinformatics.util.DateTimeUtil;
-
-import junit.framework.Assert;
 
 public class DateTimeUtilTest {
 
@@ -58,12 +56,14 @@ public class DateTimeUtilTest {
 	expected.set(Calendar.MONTH, 3);
 	expected.set(Calendar.DATE, 4);
 	Date actual = DateTimeUtil.fromSqlDateString("2018-04-04");
-	Assert.assertEquals("", expected.getTime().getTime(), actual.getTime(),
+	assertEquals("", expected.getTime().getTime(), actual.getTime(),
 		86399999);
+
     }
 
     @Test
     public final void testFromSqlDateTimeString() {
+	// should pass
 	Calendar expected = Calendar.getInstance();
 	expected.set(Calendar.YEAR, 2018);
 	expected.set(Calendar.MONTH, 3);
@@ -72,8 +72,8 @@ public class DateTimeUtilTest {
 	expected.set(Calendar.MINUTE, 12);
 	expected.set(Calendar.SECOND, 12);
 	Date actual = DateTimeUtil.fromSqlDateString("2018-04-04 12:12:12");
-	Assert.assertEquals("", expected.getTime().getTime(), actual.getTime(),
-		999);
+	assertEquals("", expected.getTime().getTime(), actual.getTime(), 999);
+
     }
 
     @Test
@@ -88,44 +88,128 @@ public class DateTimeUtilTest {
 
     @Test(expected = InvalidParameterException.class)
     public final void shouldThrowExceptionWhileDetectingFormat() {
-	Assert.assertEquals("Should throw InvalidParameterException.",
-		DateTimeUtil.detectDateFormat("11"));
-	Assert.assertEquals("Should throw InvalidParameterException.",
+	assertEquals("Should throw InvalidParameterException.",
+		DateTimeUtil.detectDateFormat("9"));
+	assertEquals("Should throw InvalidParameterException.",
+		DateTimeUtil.detectDateFormat("99"));
+	assertEquals("Should throw InvalidParameterException.",
+		DateTimeUtil.detectDateFormat("999"));
+	assertEquals("Should throw InvalidParameterException.",
 		DateTimeUtil.detectDateFormat("9999"));
-	Assert.assertEquals("Should throw InvalidParameterException.",
-		DateTimeUtil.detectDateFormat("121"));
+	assertEquals("Should throw InvalidParameterException.",
+		DateTimeUtil.detectDateFormat("9:9"));
+	assertEquals("Should throw InvalidParameterException.",
+		DateTimeUtil.detectDateFormat("99:99"));
+
     }
 
     @Test
     public final void testDetectTimeFormats() {
+	// should not pass 2859
 	List<String> times = Arrays.asList("0000", "1212", "2359", "2859");
 	String expected = "HHmm";
 	for (String time : times) {
 	    String actual = DateTimeUtil.detectDateFormat(time);
-	    Assert.assertTrue("Incorrect time format detected for " + time,
+	    assertTrue("Incorrect time format detected for " + time,
 		    actual.equals(expected));
 	}
-	Assert.assertTrue("Incorrect time format detected.",
-		DateTimeUtil.detectDateFormat("22:50").equals("HH:mm"));
-	times = Arrays.asList("00:00:00", "23:59:59", "12:00:12");
+	// should not pass 28:59
+	times = Arrays.asList("00:00", "12:12", "23:59", "28:59");
+	expected = "HH:mm";
+	for (String time : times) {
+	    String actual = DateTimeUtil.detectDateFormat(time);
+	    assertTrue("Incorrect time format detected for " + time,
+		    actual.equals(expected));
+	}
+
+	// conflict HHmmss with ddMMyy
+	times = Arrays.asList("000000", "121212", "235959", "285959", "295959");
+	expected = "HHmmss";
+	for (String time : times) {
+	    String actual = DateTimeUtil.detectDateFormat(time);
+	    assertTrue("Incorrect time format detected for " + time,
+		    actual.equals(expected));
+	}
+
+	times = Arrays.asList("00:00:00", "12:12:12", "23:59:59", "28:59:59",
+		"29:59:59");
 	expected = "HH:mm:ss";
 	for (String time : times) {
 	    String actual = DateTimeUtil.detectDateFormat(time);
-	    Assert.assertTrue("Incorrect time format detected for " + time,
+	    assertTrue("Incorrect time format detected for " + time,
 		    actual.equals(expected));
 	}
-	Assert.assertTrue("Incorrect time format detected.",
-		DateTimeUtil.detectDateFormat("235959").equals("HHmmss"));
+
     }
 
     @Test
     public final void testDetectDateFormats() {
-	fail("Not yet implemented"); // TODO
+	// Should not pass 32/13/2000
+	List<String> dates = Arrays.asList("12/12/2012", "31/12/2000",
+		"32/13/2000");
+	String expected = "dd/MM/yyyy";
+	for (String date : dates) {
+	    String actual = DateTimeUtil.detectDateFormat(date);
+	    assertTrue("Incorrect time format detected for " + date,
+		    actual.equals(expected));
+	}
+	// Should not pass 32-13-2000
+	dates = Arrays.asList("12-12-2012", "31-12-2000", "32-13-2000");
+	expected = "dd-MM-yyyy";
+	for (String date : dates) {
+	    String actual = DateTimeUtil.detectDateFormat(date);
+	    assertTrue("Incorrect time format detected for " + date,
+		    actual.equals(expected));
+	}
+
+	// Should not pass 2000-13-32
+	dates = Arrays.asList("2012-12-12", "2000-12-31", "2000-13-32");
+	expected = "yyyy-MM-dd";
+	for (String date : dates) {
+	    String actual = DateTimeUtil.detectDateFormat(date);
+	    assertTrue("Incorrect time format detected for " + date,
+		    actual.equals(expected));
+	}
+
+	// Should not pass 20001332
+	dates = Arrays.asList("20121212", "20001231", "20001332");
+	expected = "yyyyMMdd";
+	for (String date : dates) {
+	    String actual = DateTimeUtil.detectDateFormat(date);
+	    assertTrue("Incorrect time format detected for " + date,
+		    actual.equals(expected));
+	}
+
+	// conflict dd/MM/yyyy with MM/dd/yyyy
+	dates = Arrays.asList("12/12/2012", "12/31/2000");
+	expected = "MM/dd/yy";
+	for (String date : dates) {
+	    String actual = DateTimeUtil.detectDateFormat(date);
+	    assertTrue("Incorrect time format detected for " + date,
+		    actual.equals(expected));
+	}
+
+	// conflict dd/MM/yy with MM/dd/yy
+	dates = Arrays.asList("12/12/12");
+	expected = "MM/dd/yy";
+	for (String date : dates) {
+	    String actual = DateTimeUtil.detectDateFormat(date);
+	    assertTrue("Incorrect time format detected for " + date,
+		    actual.equals(expected));
+	}
     }
 
     @Test
     public final void testDetectDateTimeFormats() {
-	fail("Not yet implemented"); // TODO
+	List<String> dates = Arrays.asList("31/12/2000 23:59:59",
+		"12/12/2012 00:00:00", "32/13/2000 28:59:59");
+	String expected = "dd/MM/yyyy HH:mm:ss";
+	for (String date : dates) {
+	    String actual = DateTimeUtil.detectDateFormat(date);
+	    System.out.println(date);
+	    assertTrue("Incorrect time format detected for " + date,
+		    actual.equals(expected));
+	}
     }
 
 }
